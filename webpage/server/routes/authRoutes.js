b5c1c68b1a6c7b698cd0a625769ec5890de14311
Calvin Shi <cshi6308@umd.edu>
2025-02-08 21:43:57 -0500

    cors

--- webpage/server/routes/authRoutes.js
+++ webpage/server/routes/authRoutes.js
@@ -10,7 +10,7 @@ const { authenticateUser } = require("../controllers/userController");
 router.use(
     cors({
         credentials: true,
-        origin: "http://localhost:5173",
+        origin: "https://cover-clever.vercel.app",
         exposedHeaders: ['Authorization']  // Important for token access
     })
 );
